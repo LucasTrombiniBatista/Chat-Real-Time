503c4db394b8c17359ccaf0efe22bf94bfc7021a
Lucas Trombini <lucas.trombini@outlook.com>
2025-04-23 11:55:27 -0300

    change server url

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -98,9 +98,8 @@ const handleLogin = (event) => {
     login.style.display = "none";
     chat.style.display = "flex";
     
-    websocket = new WebSocket("ws://localhost:8080");
+    websocket = new WebSocket("wss://chat-real-time-zq6f.onrender.com");
     websocket.onmessage = processMenssage;
-    
 }
 
 const sendMessage = (event) => {
